0e50ae3c6205569da482f256bc985199df9966fe
RallyDevelopmentZone <165271002+RallyDevelopmentZone@users.noreply.github.com>
2024-03-28 15:25:59 +0200

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -30,11 +30,11 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
             .setAuthor({
-          name: 'Owner',
+          name: 'Owners',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ We Are Rally Horus Student activity supported by AAST-Entrepreneurshipcenter aims to gather, inspire, train, and mentor the next generation of innovative students.\n Facebook : ❤️ [Rally-Horus University](${youtubeLink})\n Instagram : 💙 [rally_hue](${InstagramLink})`)
+            .setDescription(`__**About us**__:\n\n ▶️ We Are Rally Horus Student activity supported by AAST-Entrepreneurshipcenter aims to gather, inspire, train, and mentor the next generation of innovative students.\n Facebook : ❤️ [Rally-Horus University](${youtubeLink})\n Instagram : 💙 [rally_hue](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
